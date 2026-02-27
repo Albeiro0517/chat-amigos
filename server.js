1c57dd32c5df5677786f05cd417af2047469ba8b
Albeiro <albeiroibarguenmurillo@hotmail.com>
2026-02-27 17:20:06 +0100

    Primer subida del chat

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+// Para poder recibir imágenes, permitimos objetos JSON grandes
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+
+// Servir archivos estáticos (index.html, icon.png, sw.js, manifest.json)
+app.use(express.static(__dirname));
+
+// Ruta principal
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+// Socket.IO
+io.on("connection", (socket) => {
+  console.log("Un usuario se conectó");
+
+  // Escuchar mensajes de chat (texto o imagen)
+  socket.on("chat message", (data) => {
+    // data.msg puede ser texto o HTML con <img>
+    io.emit("chat message", data); // enviar a todos
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Un usuario se desconectó");
+  });
+});
+
+// Puerto
+const PORT = 3000;
+http.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
